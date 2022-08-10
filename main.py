ae8fa4e18a74b3bd31079b0fc5c86338621b52aa
Diana <dicomeinit@gmail.com>
2022-08-10 15:07:58 +0300

    Add the Scoreboard and Game Over sequence

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -26,13 +27,14 @@ while game_is_on:
     for car in car_manager.all_car:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
-    # Detect collision with finish line
+    # Detect successful crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
-    # Detect successful crossing
 
 
 screen.exitonclick()
